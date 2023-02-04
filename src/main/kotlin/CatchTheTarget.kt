de97fc469ada157c35293c02ce0f4e0c595840a5
Justin Johnson <jj@MacBook-Pro.local>
2023-02-04 08:49:26 -0700

    Initial cleanup

---
+++ src/main/kotlin/CatchTheTarget.kt
@@ -0,0 +1,15 @@
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+
+fun main() = application {
+    configure {
+        width = 768
+        height = 576
+    }
+
+    program {
+        extend {
+            drawer.fill = ColorRGBa.BLACK
+        }
+    }
+}
